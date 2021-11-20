b1160f517b94de5e90ee7d5e117c53c1ccd68bf6
Anthony Magner <anthony.magner@gmail.com>
2021-11-19 23:35:15 -0700

    test

---
+++ dsl/build_cleanup.groovy
@@ -0,0 +1,23 @@
+print("Making job - ${JOB_ROOT}/branch-cleanup")
+pipelineJob("${JOB_ROOT}/branch-cleanup") {
+
+    description("Cleanup job for ${JOB_ROOT}")
+
+    /*environmentVariables {
+        env('SOURCE_BRANCH', "${gitlabSourceBranch}")
+        keepBuildVariables(true)
+    }*/
+
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote { url('http://gitlab.antlinux.local:30080/antman/pipeline-proto-helper.git') }
+                    branches('main')
+                    scriptPath('cleanup/Jenkinsfile')
+                    extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+                }
+            }
+        }
+    }
+}
