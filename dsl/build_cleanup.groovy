1741b6f19e1fe14dfb1647fd993c128478d8a693
Anthony Magner <anthony.magner@gmail.com>
2021-11-20 00:56:49 -0700

    test

--- dsl/build_cleanup.groovy
+++ dsl/build_cleanup.groovy
@@ -18,7 +18,7 @@ pipelineJob("${JOB_ROOT}/branch-cleanup") {
         pipelineTriggers {
             triggers {
                 cron {
-                    spec("H/1 * * * * ")
+                    spec("H/2 * * * * ")
                 }
             }
         }
