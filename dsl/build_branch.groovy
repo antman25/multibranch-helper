b32715ffe0932eb5e820b0124b9eb92870776b1d
Anthony Magner <anthony.magner@gmail.com>
2021-11-20 03:06:58 -0700

    test

--- dsl/build_branch.groovy
+++ dsl/build_branch.groovy
@@ -1,5 +1,9 @@
 def build_root = "/pipeline-${gitlabSourceRepoName}"
 
+def build = Thread.currentThread().executable
+def test_script_path = build.buildVariableResolver.resolve("SCRIPT_PATH")
+print("Test path: ${test_script_path}")
+
 folder("${build_root}")
 folder("${build_root}/${gitlabSourceBranch}")
 pipelineJob("${build_root}/${gitlabSourceBranch}/main-pipeline") {
