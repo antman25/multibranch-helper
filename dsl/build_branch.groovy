6cf7cbf8062bd027e76b87e2a4bc915bae98f2d9
Anthony Magner <anthony.magner@gmail.com>
2021-11-25 11:01:31 -0700

    test

--- dsl/build_branch.groovy
+++ dsl/build_branch.groovy
@@ -17,6 +17,25 @@ active_branches_split.each { cur_branch ->
         displayName("000 - Main Pipeline")
         description("Pipeline for ${gitlabSourceRepoName}")
 
+        /*
+        gitlabActionType=PUSH
+        gitlabAfter=c386efc2218594f95a8ac2c794321bb7397c1ca2
+        gitlabBefore=df7eec621763b9a8219c45e307dba91121675ac1
+        gitlabBranch=main
+        gitlabMergeRequestLastCommit=c386efc2218594f95a8ac2c794321bb7397c1ca2
+        GITLAB_OBJECT_KIND=none
+        gitlabSourceBranch=main
+        gitlabSourceNamespace=antman
+        gitlabSourceRepoHomepage=http://gitlab.antlinux.local:30080/antman/pipeline-proto-helper
+        gitlabSourceRepoHttpUrl=http://gitlab.antlinux.local:30080/antman/pipeline-proto-helper.git
+        gitlabSourceRepoName=pipeline-proto-helper
+        gitlabSourceRepoSshUrl=git@gitlab.antlinux.local:antman/pipeline-proto-helper.git
+        gitlabSourceRepoURL=git@gitlab.antlinux.local:antman/pipeline-proto-helper.git
+        gitlabTargetBranch=main
+        gitlabUserName=antman
+        gitlabUserUsername=antman
+         */
+
         environmentVariables {
             env('SOURCE_BRANCH', "${cur_branch}")
             keepBuildVariables(true)
