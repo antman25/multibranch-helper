43136e6beef966b7d23e8cba5043dbadfb54c3b8
Anthony Magner <anthony.magner@gmail.com>
2021-11-25 11:54:59 -0700

    test

--- dsl/build_branch.groovy
+++ dsl/build_branch.groovy
@@ -17,25 +17,6 @@ active_branches_split.each { cur_branch ->
         displayName("000 - Main Pipeline")
         description("Pipeline for ${gitlabSourceRepoName}")
 
-        /*
-        gitlabActionType=PUSH
-        gitlabAfter=c386efc2218594f95a8ac2c794321bb7397c1ca2
-        gitlabBefore=df7eec621763b9a8219c45e307dba91121675ac1
-        gitlabBranch=main
-        gitlabMergeRequestLastCommit=c386efc2218594f95a8ac2c794321bb7397c1ca2
-        GITLAB_OBJECT_KIND=none
-        gitlabSourceBranch=main
-        gitlabSourceNamespace=antman
-        gitlabSourceRepoHomepage=http://gitlab.antlinux.local:30080/antman/pipeline-proto-helper
-        gitlabSourceRepoHttpUrl=http://gitlab.antlinux.local:30080/antman/pipeline-proto-helper.git
-        gitlabSourceRepoName=pipeline-proto-helper
-        gitlabSourceRepoSshUrl=git@gitlab.antlinux.local:antman/pipeline-proto-helper.git
-        gitlabSourceRepoURL=git@gitlab.antlinux.local:antman/pipeline-proto-helper.git
-        gitlabTargetBranch=main
-        gitlabUserName=antman
-        gitlabUserUsername=antman
-         */
-
         parameters {
             stringParam('gitlabActionType', String.valueOf(gitlabActionType))
             stringParam('gitlabAfter', String.valueOf(gitlabAfter))
@@ -79,28 +60,3 @@ active_branches_split.each { cur_branch ->
         }
     }
 }
-
-
-/*folder("${build_root}/${gitlabSourceBranch}")
-pipelineJob("${build_root}/${gitlabSourceBranch}/main-pipeline") {
-    displayName("000 - Main Pipeline")
-    description("Pipeline for ${gitlabSourceRepoName}")
-
-    environmentVariables {
-        env('SOURCE_BRANCH', "${gitlabSourceBranch}")
-        keepBuildVariables(true)
-    }
-
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote { url("${gitlabSourceRepoHttpUrl}") }
-                    branches("${gitlabSourceBranch}")
-                    scriptPath("${SCRIPT_PATH}")
-                    extensions { }  // required as otherwise it may try to tag the repo, which you may not want
-                }
-            }
-        }
-    }
-}*/
